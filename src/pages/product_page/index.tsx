191f9db5f42fffc2cefab709064e3699b8effc93
Matvei <matvesch@protonmail.com>
2023-03-19 20:39:31 +0200

    basics

--- src/pages/product_page/index.tsx
+++ src/pages/product_page/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './ProductPage.module.scss';
 import Rates from '@/pages/product_page/sections/rates_section';
+import ProductHero from '@/pages/product_page/sections/producutHero';
 const Index = () => {
   return (
     <>
+      <ProductHero />
       <Rates />
     </>
   );
--- src/pages/product_page/sections/producutHero/index.tsx
+++ src/pages/product_page/sections/producutHero/index.tsx
@@ -1,6 +1,30 @@
-import React from "react";
-import styles from "./index.module.scss";
+import React from 'react';
+import styles from './index.module.scss';
+import Image from 'next/image';
+import playIcon from '@/media/icons/playIcon.svg';
 const ProductHero = () => {
-  return <>hello</>;
+  return (
+    <>
+      <div className={styles.actionBlock}>
+        <div
+          style={{
+            background: 'linear-gradient(270deg, #9E2FFF 0%, #FF9E2F 100%)',
+            opacity: '0.5',
+            borderRadius: '8px',
+            width: '300px',
+            height: '175px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Image src={playIcon} alt={'play-icon'} />
+        </div>
+        <div className={styles.buttonsContainer}>
+          <button>דאמו</button>
+          <button>קנייה</button>
+        </div>
+      </div>
+    </>
+  );
 };
 export default ProductHero;
